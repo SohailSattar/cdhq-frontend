1c4772b6749f34ed5b89f1cba408be229044da0f
Sohail Abdul Sattar <sohail.abdulsattar@hotmail.com>
2023-04-25 09:12:40 +0400

    renamed button text

--- src/components/CheckboxedTree/index.tsx
+++ src/components/CheckboxedTree/index.tsx
@@ -5,64 +5,70 @@ import "react-checkbox-tree/lib/react-checkbox-tree.css";
 import { Button } from "..";
 
 import "./styles.scss";
+import { useTranslation } from "react-i18next";
 
 export interface Node {
-  value: string;
-  label: string;
-  children?: Node[];
+	value: string;
+	label: string;
+	children?: Node[];
 }
 
 export interface Props {
-  direction?: string;
-  nodes: Node[];
-  onNodeCheck: (checkedNodes: string[]) => void;
+	direction?: string;
+	nodes: Node[];
+	onNodeCheck: (checkedNodes: string[]) => void;
 }
 
 const CheckboxedTree: FC<Props> = ({
-  nodes,
-  direction = "rtl",
-  onNodeCheck,
+	nodes,
+	direction = "rtl",
+	onNodeCheck,
 }) => {
-  const [checked, setChecked] = useState<string[]>([]); // '110000000'
-  const [expanded, setExpanded] = useState<string[]>([]);
+	const [t] = useTranslation("common");
+	const [checked, setChecked] = useState<string[]>([]); // '110000000'
+	const [expanded, setExpanded] = useState<string[]>([]);
 
-  const [noCascade, setNoCascade] = useState(false);
+	const [noCascade, setNoCascade] = useState(false);
 
-  const checkHandler = (checked: string[], node: any) => {
-    setChecked(checked);
-    onNodeCheck(checked);
-  };
+	const checkHandler = (checked: string[], node: any) => {
+		setChecked(checked);
+		onNodeCheck(checked);
+	};
 
-  const cascadeButtonClickHandler = () => {
-    setNoCascade(!noCascade);
-  };
+	const cascadeButtonClickHandler = () => {
+		setNoCascade(!noCascade);
+	};
 
-const clearButtonHandler = ()=>{
-	setChecked([])
-}
+	const clearButtonHandler = () => {
+		setChecked([]);
+	};
 
-  return (
-    <div className="checkTree">
-      <div className="btnSection">
-        <Button onClick={cascadeButtonClickHandler}>
-          Select Child: {!noCascade ? "On" : "Off"}
-        </Button>
-		<Button isRound onClick={clearButtonHandler}>Clear</Button>
-      </div>
-      <CheckboxTree
-        nodes={nodes}
-        checked={checked}
-        expanded={expanded}
-        onCheck={checkHandler}
-        onExpand={setExpanded}
-        checkModel="all"
-        direction={direction}
-        nameAsArray={true}
-        noCascade={noCascade}
-        showNodeIcon={false}
-      />
-    </div>
-  );
+	return (
+		<div className="checkTree">
+			<div className="btnSection">
+				<Button onClick={cascadeButtonClickHandler}>
+					Select Child: {!noCascade ? "On" : "Off"}
+				</Button>
+				<Button
+					isRound
+					onClick={clearButtonHandler}>
+					{t("button.clear", { framework: "React" })}
+				</Button>
+			</div>
+			<CheckboxTree
+				nodes={nodes}
+				checked={checked}
+				expanded={expanded}
+				onCheck={checkHandler}
+				onExpand={setExpanded}
+				checkModel="all"
+				direction={direction}
+				nameAsArray={true}
+				noCascade={noCascade}
+				showNodeIcon={false}
+			/>
+		</div>
+	);
 };
 
 export default CheckboxedTree;
