a73f18f3f9b561a02b1e6ff81955fc0a37ee9ac8
Sohail Abdul Sattar <sohail.abdulsattar@hotmail.com>
2023-07-07 09:59:30 +0400

    added for project

--- src/api/projects/get/getProjects.ts
+++ src/api/projects/get/getProjects.ts
@@ -6,18 +6,28 @@ import { APIPaginatedProject } from "../types";
 export async function getProjects(
 	currentPage: number,
 	pageSize: number,
-	statusCode?: Id
+	keyword?: string,
+	statusCode?: Id,
+	orderBy?: string
 ): Promise<APIResponse<APIPaginatedProject>> {
 	try {
 		const config = getConfig();
 
-		let codeParameter = "";
+		let queryParam = "";
 
-		if (statusCode) {
-			codeParameter = `&statuscode=${statusCode}`;
+		if (keyword) {
+			queryParam += `&keyword=${keyword}`;
 		}
 
-		const url = `/projects?page=${currentPage}&postsperpage=${pageSize}${codeParameter}`;
+		if (statusCode) {
+			queryParam += `&statuscode=${statusCode}`;
+		}
+
+		if (orderBy) {
+			queryParam += `${orderBy}`;
+		}
+
+		const url = `/projects?page=${currentPage}&postsperpage=${pageSize}${queryParam}`;
 
 		const response = await instance.get<APIPaginatedProject>(url, config);
 		const data = response.data;
