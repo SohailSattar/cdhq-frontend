0056d03a24f829e6bacebdc803964b2ae7f63fbb
Sohail Abdul Sattar <sohail.abdulsattar@hotmail.com>
2023-09-07 12:54:05 +0400

    news

--- src/components/News/Detail/index.tsx
+++ src/components/News/Detail/index.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
-import { NewsContent, NewsHeading, NewsMeta, ShadowedContainer } from "../..";
+import { motion } from "framer-motion";
+import {
+	Hr,
+	NewsContent,
+	NewsHeading,
+	NewsMeta,
+	ShadowedContainer,
+} from "../..";
 import { APINewsDetail } from "../../../api/news/types";
 
 import styles from "./styles.module.scss";
+import clsx from "clsx";
 
 interface Props {
 	detail: APINewsDetail;
@@ -11,17 +19,42 @@ interface Props {
 const NewsDetail: FC<Props> = ({ detail }) => {
 	return (
 		<>
-			<ShadowedContainer className={styles.container}>
+			<motion.div
+				className={styles.container}
+				initial={{ opacity: 0, y: 50 }} // Adjust initial position and opacity
+				animate={{ opacity: 1, y: 0 }} // Adjust animation to y instead of x
+				exit={{ opacity: 0, y: -50 }} // Adjust exit to y instead of x
+				transition={{
+					type: "spring",
+					damping: 10,
+					stiffness: 500,
+					duration: 1,
+				}}>
 				<NewsHeading title={detail?.title!} />
-			</ShadowedContainer>
+			</motion.div>
+			<Hr />
 			<div className={styles.container}>
 				<NewsMeta postedDate={new Date(detail?.newsDate!).toDateString()} />
 			</div>
-			<ShadowedContainer className={styles.container}>
-				<NewsContent
-					imagePath={detail?.imageName}
-					data={detail?.fullNews!}
-				/>
+			<Hr />
+
+			<ShadowedContainer className={clsx(styles.content)}>
+				<motion.div
+					className="container"
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}>
+					<motion.div
+						initial={{ opacity: 0, y: -10 }} // Adjust initial opacity and position
+						animate={{ opacity: 1, y: 0 }} // Adjust animation properties
+						exit={{ opacity: 0, y: 10 }} // Adjust exit properties
+					>
+						<NewsContent
+							imagePath={detail?.imageName}
+							data={detail?.fullNews!}
+						/>
+					</motion.div>
+				</motion.div>
 			</ShadowedContainer>
 		</>
 	);
