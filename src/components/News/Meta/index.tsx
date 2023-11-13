5835f18aae81d7e49e3ac3068b816fa9021b711b
Sohail Abdul Sattar <sohail.abdulsattar@hotmail.com>
2023-11-13 10:58:22 +0400

    changed some layout related to shadowed and paginated table

--- src/components/News/Meta/index.tsx
+++ src/components/News/Meta/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 
 import styles from "./styles.module.scss";
+import { useTranslation } from "react-i18next";
 
 interface Props {
 	postedDate: string;
 }
 
 const NewsMeta: FC<Props> = ({ postedDate }) => {
-	return <>Dated: {postedDate} </>;
+	const [t] = useTranslation("common");
+	return (
+		<div className={styles.container}>
+			{t("common.dated", { framework: "React" })}: {postedDate}{" "}
+		</div>
+	);
 };
 
 export default NewsMeta;
